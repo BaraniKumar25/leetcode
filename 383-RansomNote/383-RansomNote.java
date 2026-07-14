b0916b6f04d278279cb67e5bd0a088976523ee64
BaraniKumar25 <baranikumarsn@gmail.com>
2026-07-14 10:10:59 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.java
@@ -0,0 +1,22 @@
+// Last updated: 7/14/2026, 10:10:59 AM
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        int[] count = new int[26];
+
+        for (char c : magazine.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+
+            if (count[c - 'a'] == 0) {
+                return false;
+            }
+
+            count[c - 'a']--;
+        }
+
+        return true;
+    }
+}
