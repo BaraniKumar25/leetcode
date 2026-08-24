dfec432866154b1c22457327b8b1a9386c1803fb
BaraniKumar25 <baranikumarsn@gmail.com>
2026-08-24 09:29:37 +0530

    File updated at 8/24/2026, 9:29:37 AM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
@@ -1,35 +1,22 @@
-# Last updated: 8/24/2026, 9:29:20 AM
-1from collections import deque
+# Last updated: 8/24/2026, 9:29:37 AM
+1class Solution:
 2
-3
-4class Solution:
-5
-6  def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-7    if not root:
-8      return []
-9
-10    res = []
-11    queue = deque([root])
-12    left_to_right = True
+3  def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+4    min_diff = float('inf')
+5    prev = None
+6
+7    def inorder(node):
+8      nonlocal min_diff, prev
+9      if not node:
+10        return
+11
+12      inorder(node.left)
 13
-14    while queue:
-15      level_size = len(queue)
-16      level = deque()
+14      if prev is not None:
+15        min_diff = min(min_diff, node.val - prev)
+16      prev = node.val
 17
-18      for _ in range(level_size):
-19        node = queue.popleft()
-20
-21        if left_to_right:
-22          level.append(node.val)
-23        else:
-24          level.appendleft(node.val)
-25
-26        if node.left:
-27          queue.append(node.left)
-28        if node.right:
-29          queue.append(node.right)
-30
-31      res.append(list(level))
-32      left_to_right = not left_to_right
-33
-34    return res
+18      inorder(node.right)
+19
+20    inorder(root)
+21    return min_diff
