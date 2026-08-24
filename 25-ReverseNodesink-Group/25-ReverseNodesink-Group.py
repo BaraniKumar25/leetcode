c0dc4143875cf26f7965f8758c4b84d610461fb8
BaraniKumar25 <baranikumarsn@gmail.com>
2026-08-24 08:53:43 +0530

    File updated at 8/24/2026, 8:53:40 AM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
@@ -1,24 +1,24 @@
-# Last updated: 8/24/2026, 8:53:13 AM
+# Last updated: 8/24/2026, 8:53:40 AM
 1class Solution:
 2
 3  def buildTree(
-4      self, preorder: List[int], inorder: List[int]
+4      self, inorder: List[int], postorder: List[int]
 5  ) -> Optional[TreeNode]:
 6    inorder_map = {val: idx for idx, val in enumerate(inorder)}
-7    pre_idx = 0
+7    post_idx = len(postorder) - 1
 8
 9    def helper(left, right):
-10      nonlocal pre_idx
+10      nonlocal post_idx
 11      if left > right:
 12        return None
 13
-14      root_val = preorder[pre_idx]
+14      root_val = postorder[post_idx]
 15      root = TreeNode(root_val)
-16      pre_idx += 1
+16      post_idx -= 1
 17
 18      mid = inorder_map[root_val]
-19      root.left = helper(left, mid - 1)
-20      root.right = helper(mid + 1, right)
+19      root.right = helper(mid + 1, right)
+20      root.left = helper(left, mid - 1)
 21
 22      return root
 23
