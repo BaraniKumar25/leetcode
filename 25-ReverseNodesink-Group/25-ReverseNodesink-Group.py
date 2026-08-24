bd773c639a9b38024cb5918f245fb8f1d733a3c7
BaraniKumar25 <baranikumarsn@gmail.com>
2026-08-24 08:49:41 +0530

    File updated at 8/24/2026, 8:49:41 AM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
@@ -1,28 +1,23 @@
-# Last updated: 8/24/2026, 8:49:16 AM
+# Last updated: 8/24/2026, 8:49:41 AM
 1class Solution:
 2
-3  def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-4    if not head or not head.next or k == 0:
-5      return head
+3  def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+4    before_head = ListNode(0)
+5    after_head = ListNode(0)
 6
-7    length = 1
-8    tail = head
-9    while tail.next:
-10      tail = tail.next
-11      length += 1
-12
-13    k = k % length
-14    if k == 0:
-15      return head
-16
-17    tail.next = head
+7    before = before_head
+8    after = after_head
+9
+10    while head:
+11      if head.val < x:
+12        before.next = head
+13        before = before.next
+14      else:
+15        after.next = head
+16        after = after.next
+17      head = head.next
 18
-19    steps_to_new_tail = length - k
-20    new_tail = head
-21    for _ in range(steps_to_new_tail - 1):
-22      new_tail = new_tail.next
-23
-24    new_head = new_tail.next
-25    new_tail.next = None
-26
-27    return new_head
+19    after.next = None
+20    before.next = after_head.next
+21
+22    return before_head.next
