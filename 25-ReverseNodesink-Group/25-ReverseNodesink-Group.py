c733fa1f383a6ab0994f56dd7e39a39709c36d64
BaraniKumar25 <baranikumarsn@gmail.com>
2026-08-24 08:52:27 +0530

    File updated at 8/24/2026, 8:52:27 AM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
@@ -1,13 +1,15 @@
-# Last updated: 8/24/2026, 8:52:00 AM
+# Last updated: 8/24/2026, 8:52:27 AM
 1class Solution:
 2
-3  def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+3  def isSymmetric(self, root: Optional[TreeNode]) -> bool:
 4    if not root:
-5      return None
+5      return True
 6
-7    root.left, root.right = root.right, root.left
-8
-9    self.invertTree(root.left)
-10    self.invertTree(root.right)
-11
-12    return root
+7    def is_mirror(t1, t2):
+8      if not t1 and not t2:
+9        return True
+10      if not t1 or not t2 or t1.val != t2.val:
+11        return False
+12      return is_mirror(t1.left, t2.right) and is_mirror(t1.right, t2.left)
+13
+14    return is_mirror(root.left, root.right)
