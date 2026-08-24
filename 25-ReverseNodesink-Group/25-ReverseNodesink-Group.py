8d31ee59c0ed39f49f596d24e4b4a8c1b9a5bea9
BaraniKumar25 <baranikumarsn@gmail.com>
2026-08-24 09:12:20 +0530

    File updated at 8/24/2026, 9:12:19 AM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
@@ -1,16 +1,18 @@
-# Last updated: 8/24/2026, 9:11:49 AM
+# Last updated: 8/24/2026, 9:12:19 AM
 1class Solution:
 2
-3  def lowestCommonAncestor(
-4      self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode'
-5  ) -> 'TreeNode':
-6    if not root or root == p or root == q:
-7      return root
-8
-9    left = self.lowestCommonAncestor(root.left, p, q)
-10    right = self.lowestCommonAncestor(root.right, p, q)
-11
-12    if left and right:
-13      return root
-14
-15    return left if left else right
+3  def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+4    res = []
+5
+6    def dfs(node, depth):
+7      if not node:
+8        return
+9
+10      if depth == len(res):
+11        res.append(node.val)
+12
+13      dfs(node.right, depth + 1)
+14      dfs(node.left, depth + 1)
+15
+16    dfs(root, 0)
+17    return res
