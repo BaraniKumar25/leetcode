69edecb13f71c5c67f8d5d3efb4e6df11ddfad0f
BaraniKumar25 <baranikumarsn@gmail.com>
2026-08-24 08:51:41 +0530

    File updated at 8/24/2026, 8:51:41 AM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
@@ -1,10 +1,13 @@
-# Last updated: 8/24/2026, 8:51:15 AM
+# Last updated: 8/24/2026, 8:51:41 AM
 1class Solution:
 2
-3  def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-4    if not p and not q:
-5      return True
-6    if not p or not q or p.val != q.val:
-7      return False
+3  def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+4    if not root:
+5      return None
+6
+7    root.left, root.right = root.right, root.left
 8
-9    return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+9    self.invertTree(root.left)
+10    self.invertTree(root.right)
+11
+12    return root
