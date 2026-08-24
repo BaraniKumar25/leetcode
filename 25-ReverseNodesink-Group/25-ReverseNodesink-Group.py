04b582c160ccc972332679f48684ee603debc72d
BaraniKumar25 <baranikumarsn@gmail.com>
2026-08-24 08:50:32 +0530

    File updated at 8/24/2026, 8:50:32 AM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
@@ -1,52 +1,8 @@
-# Last updated: 8/24/2026, 8:50:06 AM
-1class Node:
+# Last updated: 8/24/2026, 8:50:32 AM
+1class Solution:
 2
-3  def __init__(self, key: int, val: int):
-4    self.key = key
-5    self.val = val
-6    self.prev = None
-7    self.next = None
-8
-9
-10class LRUCache:
-11
-12  def __init__(self, capacity: int):
-13    self.cap = capacity
-14    self.cache = {}
-15    self.head = Node(0, 0)
-16    self.tail = Node(0, 0)
-17    self.head.next = self.tail
-18    self.tail.prev = self.head
-19
-20  def _remove(self, node: Node):
-21    prev, nxt = node.prev, node.next
-22    prev.next = nxt
-23    nxt.prev = prev
-24
-25  def _add(self, node: Node):
-26    prev, nxt = self.tail.prev, self.tail
-27    prev.next = node
-28    node.prev = prev
-29    node.next = nxt
-30    nxt.prev = node
-31
-32  def get(self, key: int) -> int:
-33    if key in self.cache:
-34      node = self.cache[key]
-35      self._remove(node)
-36      self._add(node)
-37      return node.val
-38    return -1
-39
-40  def put(self, key: int, value: int) -> None:
-41    if key in self.cache:
-42      self._remove(self.cache[key])
-43
-44    node = Node(key, value)
-45    self._add(node)
-46    self.cache[key] = node
-47
-48    if len(self.cache) > self.cap:
-49      lru = self.head.next
-50      self._remove(lru)
-51      del self.cache[lru.key]
+3  def maxDepth(self, root: Optional[TreeNode]) -> int:
+4    if not root:
+5      return 0
+6
+7    return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
