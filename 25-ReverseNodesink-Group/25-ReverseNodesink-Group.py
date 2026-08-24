38b1355dd0a427204c93621cd6efc0702d88c1d3
BaraniKumar25 <baranikumarsn@gmail.com>
2026-08-24 08:53:14 +0530

    File updated at 8/24/2026, 8:53:13 AM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
@@ -1,15 +1,25 @@
-# Last updated: 8/24/2026, 8:52:27 AM
+# Last updated: 8/24/2026, 8:53:13 AM
 1class Solution:
 2
-3  def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-4    if not root:
-5      return True
-6
-7    def is_mirror(t1, t2):
-8      if not t1 and not t2:
-9        return True
-10      if not t1 or not t2 or t1.val != t2.val:
-11        return False
-12      return is_mirror(t1.left, t2.right) and is_mirror(t1.right, t2.left)
+3  def buildTree(
+4      self, preorder: List[int], inorder: List[int]
+5  ) -> Optional[TreeNode]:
+6    inorder_map = {val: idx for idx, val in enumerate(inorder)}
+7    pre_idx = 0
+8
+9    def helper(left, right):
+10      nonlocal pre_idx
+11      if left > right:
+12        return None
 13
-14    return is_mirror(root.left, root.right)
+14      root_val = preorder[pre_idx]
+15      root = TreeNode(root_val)
+16      pre_idx += 1
+17
+18      mid = inorder_map[root_val]
+19      root.left = helper(left, mid - 1)
+20      root.right = helper(mid + 1, right)
+21
+22      return root
+23
+24    return helper(0, len(inorder) - 1)
