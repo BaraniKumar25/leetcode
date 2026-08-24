d6b5c984c0daa650a7464dde1e3b21a74f10739a
BaraniKumar25 <baranikumarsn@gmail.com>
2026-08-24 08:48:28 +0530

    File updated at 8/24/2026, 8:48:28 AM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
@@ -1,34 +1,20 @@
-# Last updated: 8/24/2026, 8:48:04 AM
+# Last updated: 8/24/2026, 8:48:28 AM
 1class Solution:
 2
-3  def reverseKGroup(
-4      self, head: Optional[ListNode], k: int
+3  def removeNthFromEnd(
+4      self, head: Optional[ListNode], n: int
 5  ) -> Optional[ListNode]:
 6    dummy = ListNode(0, head)
-7    group_prev = dummy
-8
-9    while True:
-10      kth = self.get_kth(group_prev, k)
-11      if not kth:
-12        break
-13
-14      group_next = kth.next
-15      prev, curr = kth.next, group_prev.next
+7    fast = head
+8    slow = dummy
+9
+10    for _ in range(n):
+11      fast = fast.next
+12
+13    while fast:
+14      fast = fast.next
+15      slow = slow.next
 16
-17      while curr != group_next:
-18        tmp = curr.next
-19        curr.next = prev
-20        prev = curr
-21        curr = tmp
-22
-23      tmp = group_prev.next
-24      group_prev.next = kth
-25      group_prev = tmp
-26
-27    return dummy.next
-28
-29  def get_kth(self, curr, k):
-30    while curr and k > 0:
-31      curr = curr.next
-32      k -= 1
-33    return curr
+17    slow.next = slow.next.next
+18
+19    return dummy.next
