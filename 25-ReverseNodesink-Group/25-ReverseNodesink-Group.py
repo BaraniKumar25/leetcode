ab1318b6117aa2d9c536b82a7fa0ddeaeff7a26b
BaraniKumar25 <baranikumarsn@gmail.com>
2026-08-24 08:57:12 +0530

    File updated at 8/24/2026, 8:57:11 AM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
@@ -1,17 +1,15 @@
-# Last updated: 8/24/2026, 8:55:34 AM
+# Last updated: 8/24/2026, 8:57:11 AM
 1class Solution:
 2
-3  def flatten(self, root: Optional[TreeNode]) -> None:
-4    curr = root
-5
-6    while curr:
-7      if curr.left:
-8        prev = curr.left
-9        while prev.right:
-10          prev = prev.right
+3  def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+4    if not root:
+5      return False
+6
+7    if not root.left and not root.right:
+8      return targetSum == root.val
+9
+10    targetSum -= root.val
 11
-12        prev.right = curr.right
-13        curr.right = curr.left
-14        curr.left = None
-15
-16      curr = curr.right
+12    return self.hasPathSum(root.left, targetSum) or self.hasPathSum(
+13        root.right, targetSum
+14    )
