224cdf5b3c26169ba6d2cbabe7742c7906e9cd88
BaraniKumar25 <baranikumarsn@gmail.com>
2026-08-24 08:48:53 +0530

    File updated at 8/24/2026, 8:48:53 AM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
@@ -1,20 +1,17 @@
-# Last updated: 8/24/2026, 8:48:28 AM
+# Last updated: 8/24/2026, 8:48:53 AM
 1class Solution:
 2
-3  def removeNthFromEnd(
-4      self, head: Optional[ListNode], n: int
-5  ) -> Optional[ListNode]:
-6    dummy = ListNode(0, head)
-7    fast = head
-8    slow = dummy
-9
-10    for _ in range(n):
-11      fast = fast.next
-12
-13    while fast:
-14      fast = fast.next
-15      slow = slow.next
-16
-17    slow.next = slow.next.next
-18
-19    return dummy.next
+3  def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+4    dummy = ListNode(0, head)
+5    prev = dummy
+6
+7    while head:
+8      if head.next and head.val == head.next.val:
+9        while head.next and head.val == head.next.val:
+10          head = head.next
+11        prev.next = head.next
+12      else:
+13        prev = prev.next
+14      head = head.next
+15
+16    return dummy.next
