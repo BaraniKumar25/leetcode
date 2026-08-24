23217fba61ca0b69b9a754838dfc1538cc7442ff
BaraniKumar25 <baranikumarsn@gmail.com>
2026-08-24 09:27:05 +0530

    File updated at 8/24/2026, 9:27:04 AM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
@@ -1,25 +1,27 @@
-# Last updated: 8/24/2026, 9:12:41 AM
+# Last updated: 8/24/2026, 9:27:04 AM
 1from collections import deque
 2
 3
 4class Solution:
 5
-6  def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
-7    res = []
-8    queue = deque([root])
+6  def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+7    if not root:
+8      return []
 9
-10    while queue:
-11      level_sum = 0
-12      level_count = len(queue)
-13
-14      for _ in range(level_count):
-15        node = queue.popleft()
-16        level_sum += node.val
-17        if node.left:
-18          queue.append(node.left)
-19        if node.right:
-20          queue.append(node.right)
-21
-22      res.append(level_sum / level_count)
+10    res = []
+11    queue = deque([root])
+12
+13    while queue:
+14      level = []
+15      for _ in range(len(queue)):
+16        node = queue.popleft()
+17        level.append(node.val)
+18
+19        if node.left:
+20          queue.append(node.left)
+21        if node.right:
+22          queue.append(node.right)
 23
-24    return res
+24      res.append(level)
+25
+26    return res
