5df4a4e06b06350960e13e5cfba3781a3d95ad43
BaraniKumar25 <baranikumarsn@gmail.com>
2026-08-24 08:57:29 +0530

    File updated at 8/24/2026, 8:57:29 AM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
@@ -1,15 +1,16 @@
-# Last updated: 8/24/2026, 8:57:11 AM
+# Last updated: 8/24/2026, 8:57:29 AM
 1class Solution:
 2
-3  def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-4    if not root:
-5      return False
-6
-7    if not root.left and not root.right:
-8      return targetSum == root.val
+3  def sumNumbers(self, root: Optional[TreeNode]) -> int:
+4    def dfs(node, current_sum):
+5      if not node:
+6        return 0
+7
+8      current_sum = current_sum * 10 + node.val
 9
-10    targetSum -= root.val
-11
-12    return self.hasPathSum(root.left, targetSum) or self.hasPathSum(
-13        root.right, targetSum
-14    )
+10      if not node.left and not node.right:
+11        return current_sum
+12
+13      return dfs(node.left, current_sum) + dfs(node.right, current_sum)
+14
+15    return dfs(root, 0)
