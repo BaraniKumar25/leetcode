94fa14236d9d37bd907ec025d1852878c8384f44
Barani Kumar <baranikumarsn@gmail.com>
2026-08-28 11:11:22 +0530

    File updated at 8/28/2026, 11:11:23 AM

--- 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.py
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.py
@@ -1,17 +1,12 @@
-# Last updated: 8/28/2026, 11:10:49 AM
+# Last updated: 8/28/2026, 11:11:23 AM
 1class Solution:
-2    def convert(self, s: str, numRows: int) -> str:
-3        if numRows == 1 or numRows >= len(s):
-4            return s
+2    def reverse(self, x: int) -> int:
+3        sign = -1 if x < 0 else 1
+4        x_abs = abs(x)
 5        
-6        rows = [''] * numRows
-7        current_row = 0
-8        going_down = False
-9        
-10        for char in s:
-11            rows[current_row] += char
-12            if current_row == 0 or current_row == numRows - 1:
-13                going_down = not going_down
-14            current_row += 1 if going_down else -1
-15            
-16        return ''.join(rows)
+6        reversed_num = int(str(x_abs)[::-1]) * sign
+7        
+8        if reversed_num < -2**31 or reversed_num > 2**31 - 1:
+9            return 0
+10            
+11        return reversed_num
