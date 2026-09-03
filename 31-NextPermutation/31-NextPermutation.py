6947a2b747a32877ee0161889d50779be860b956
Barani Kumar <baranikumarsn@gmail.com>
2026-09-03 14:10:13 +0530

    File updated at 9/3/2026, 2:10:13 PM

--- 31-NextPermutation/31-NextPermutation.py
+++ 31-NextPermutation/31-NextPermutation.py
@@ -1,19 +1,23 @@
-# Last updated: 9/3/2026, 2:09:42 PM
+# Last updated: 9/3/2026, 2:10:13 PM
 1class Solution:
-2    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-3        res = []
-4
-5        def backtrack(start, current_combination, current_sum):
-6            if current_sum == target:
-7                res.append(list(current_combination))
-8                return
-9            if current_sum > target:
-10                return
-11
-12            for i in range(start, len(candidates)):
-13                current_combination.append(candidates[i])
-14                backtrack(i, current_combination, current_sum + candidates[i])
-15                current_combination.pop()
-16
-17        backtrack(0, [], 0)
-18        return res
+2    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+3        candidates.sort()
+4        res = []
+5
+6        def backtrack(start, current_combination, current_sum):
+7            if current_sum == target:
+8                res.append(list(current_combination))
+9                return
+10            if current_sum > target:
+11                return
+12
+13            for i in range(start, len(candidates)):
+14                if i > start and candidates[i] == candidates[i - 1]:
+15                    continue
+16                
+17                current_combination.append(candidates[i])
+18                backtrack(i + 1, current_combination, current_sum + candidates[i])
+19                current_combination.pop()
+20
+21        backtrack(0, [], 0)
+22        return res
