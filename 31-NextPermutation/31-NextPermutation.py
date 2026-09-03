def56fa581b2074989557e287ee720a8330cd631
Barani Kumar <baranikumarsn@gmail.com>
2026-09-03 14:09:43 +0530

    File updated at 9/3/2026, 2:09:42 PM

--- 31-NextPermutation/31-NextPermutation.py
+++ 31-NextPermutation/31-NextPermutation.py
@@ -1,17 +1,19 @@
-# Last updated: 9/3/2026, 2:09:11 PM
+# Last updated: 9/3/2026, 2:09:42 PM
 1class Solution:
-2    def countAndSay(self, n: int) -> str:
-3        s = "1"
-4        for _ in range(n - 1):
-5            next_s = []
-6            i = 0
-7            while i < len(s):
-8                count = 1
-9                while i + 1 < len(s) and s[i] == s[i + 1]:
-10                    count += 1
-11                    i += 1
-12                next_s.append(str(count))
-13                next_s.append(s[i])
-14                i += 1
-15            s = "".join(next_s)
-16        return s
+2    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+3        res = []
+4
+5        def backtrack(start, current_combination, current_sum):
+6            if current_sum == target:
+7                res.append(list(current_combination))
+8                return
+9            if current_sum > target:
+10                return
+11
+12            for i in range(start, len(candidates)):
+13                current_combination.append(candidates[i])
+14                backtrack(i, current_combination, current_sum + candidates[i])
+15                current_combination.pop()
+16
+17        backtrack(0, [], 0)
+18        return res
