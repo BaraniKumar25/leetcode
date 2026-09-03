5c69c58e6c5be9531191bd6d10da2c03dfb69705
Barani Kumar <baranikumarsn@gmail.com>
2026-09-03 14:49:26 +0530

    File updated at 9/3/2026, 2:49:25 PM

--- 60-PermutationSequence/60-PermutationSequence.py
+++ 60-PermutationSequence/60-PermutationSequence.py
@@ -1,18 +1,14 @@
-# Last updated: 9/3/2026, 2:48:56 PM
+# Last updated: 9/3/2026, 2:49:25 PM
 1class Solution:
-2    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-3        if not obstacleGrid or obstacleGrid[0][0] == 1:
-4            return 0
-5
-6        m, n = len(obstacleGrid), len(obstacleGrid[0])
-7        dp = [0] * n
-8        dp[0] = 1
-9
-10        for r in range(m):
-11            for c in range(n):
-12                if obstacleGrid[r][c] == 1:
-13                    dp[c] = 0
-14                elif c > 0:
-15                    dp[c] += dp[c - 1]
-16
-17        return dp[-1]
+2    def minPathSum(self, grid: List[List[int]]) -> int:
+3        m, n = len(grid), len(grid[0])
+4
+5        dp = [float("inf")] * n
+6        dp[0] = 0
+7
+8        for r in range(m):
+9            dp[0] += grid[r][0]
+10            for c in range(1, n):
+11                dp[c] = min(dp[c], dp[c - 1]) + grid[r][c]
+12
+13        return dp[-1]
