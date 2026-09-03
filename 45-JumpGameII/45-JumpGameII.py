96d0c61e4bbef17511305231cc7905a0591166dc
Barani Kumar <baranikumarsn@gmail.com>
2026-09-03 14:20:48 +0530

    File updated at 9/3/2026, 2:20:48 PM

--- 45-JumpGameII/45-JumpGameII.py
+++ 45-JumpGameII/45-JumpGameII.py
@@ -1,33 +1,29 @@
-# Last updated: 9/3/2026, 2:20:22 PM
+# Last updated: 9/3/2026, 2:20:48 PM
 1class Solution:
-2    def solveNQueens(self, n: int) -> List[List[str]]:
-3        res = []
-4        cols = set()
-5        pos_diag = set()  # (r + c)
-6        neg_diag = set()  # (r - c)
-7
-8        board = [["."] * n for _ in range(n)]
-9
-10        def backtrack(r):
-11            if r == n:
-12                res.append(["".join(row) for row in board])
-13                return
-14
-15            for c in range(n):
-16                if c in cols or (r + c) in pos_diag or (r - c) in neg_diag:
-17                    continue
-18
-19                cols.add(c)
-20                pos_diag.add(r + c)
-21                neg_diag.add(r - c)
-22                board[r][c] = "Q"
-23
-24                backtrack(r + 1)
+2    def totalNQueens(self, n: int) -> int:
+3        cols = set()
+4        pos_diag = set()  # (r + c)
+5        neg_diag = set()  # (r - c)
+6
+7        def backtrack(r):
+8            if r == n:
+9                return 1
+10
+11            count = 0
+12            for c in range(n):
+13                if c in cols or (r + c) in pos_diag or (r - c) in neg_diag:
+14                    continue
+15
+16                cols.add(c)
+17                pos_diag.add(r + c)
+18                neg_diag.add(r - c)
+19
+20                count += backtrack(r + 1)
+21
+22                cols.remove(c)
+23                pos_diag.remove(r + c)
+24                neg_diag.remove(r - c)
 25
-26                cols.remove(c)
-27                pos_diag.remove(r + c)
-28                neg_diag.remove(r - c)
-29                board[r][c] = "."
-30
-31        backtrack(0)
-32        return res
+26            return count
+27
+28        return backtrack(0)
