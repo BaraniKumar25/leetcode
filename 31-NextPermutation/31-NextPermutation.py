ecd83928347b349277dc205682bbba6f9090404b
Barani Kumar <baranikumarsn@gmail.com>
2026-09-03 14:08:21 +0530

    File updated at 9/3/2026, 2:08:20 PM

--- 31-NextPermutation/31-NextPermutation.py
+++ 31-NextPermutation/31-NextPermutation.py
@@ -1,23 +1,21 @@
-# Last updated: 9/3/2026, 2:05:42 PM
+# Last updated: 9/3/2026, 2:08:20 PM
 1class Solution:
-2    def searchRange(self, nums: List[int], target: int) -> List[int]:
-3        def findBound(isFirst: bool) -> int:
-4            left, right = 0, len(nums) - 1
-5            bound = -1
-6            
-7            while left <= right:
-8                mid = (left + right) // 2
-9                if nums[mid] == target:
-10                    bound = mid
-11                    if isFirst:
-12                        right = mid - 1
-13                    else:
-14                        left = mid + 1
-15                elif nums[mid] < target:
-16                    left = mid + 1
-17                else:
-18                    right = mid - 1
-19                    
-20            return bound
-21
-22        return [findBound(True), findBound(False)]
+2    def isValidSudoku(self, board: List[List[str]]) -> bool:
+3        cols = collections.defaultdict(set)
+4        rows = collections.defaultdict(set)
+5        squares = collections.defaultdict(set)
+6
+7        for r in range(9):
+8            for c in range(9):
+9                if board[r][c] == ".":
+10                    continue
+11                if (board[r][c] in rows[r] or
+12                    board[r][c] in cols[c] or
+13                    board[r][c] in squares[(r // 3, c // 3)]):
+14                    return False
+15                
+16                cols[c].add(board[r][c])
+17                rows[r].add(board[r][c])
+18                squares[(r // 3, c // 3)].add(board[r][c])
+19
+20        return True
