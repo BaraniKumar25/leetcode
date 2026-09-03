db284f226bbd0983f703a8ecae4f08e02277db44
Barani Kumar <baranikumarsn@gmail.com>
2026-09-03 14:25:50 +0530

    File updated at 9/3/2026, 2:25:49 PM

--- 45-JumpGameII/45-JumpGameII.py
+++ 45-JumpGameII/45-JumpGameII.py
@@ -1,29 +1,32 @@
-# Last updated: 9/3/2026, 2:20:48 PM
+# Last updated: 9/3/2026, 2:25:49 PM
 1class Solution:
-2    def totalNQueens(self, n: int) -> int:
-3        cols = set()
-4        pos_diag = set()  # (r + c)
-5        neg_diag = set()  # (r - c)
-6
-7        def backtrack(r):
-8            if r == n:
-9                return 1
-10
-11            count = 0
-12            for c in range(n):
-13                if c in cols or (r + c) in pos_diag or (r - c) in neg_diag:
-14                    continue
-15
-16                cols.add(c)
-17                pos_diag.add(r + c)
-18                neg_diag.add(r - c)
-19
-20                count += backtrack(r + 1)
-21
-22                cols.remove(c)
-23                pos_diag.remove(r + c)
-24                neg_diag.remove(r - c)
-25
-26            return count
-27
-28        return backtrack(0)
+2    def generateMatrix(self, n: int) -> List[List[int]]:
+3        matrix = [[0] * n for _ in range(n)]
+4        top, bottom = 0, n - 1
+5        left, right = 0, n - 1
+6        num = 1
+7
+8        while top <= bottom and left <= right:
+9            for col in range(left, right + 1):
+10                matrix[top][col] = num
+11                num += 1
+12            top += 1
+13
+14            for row in range(top, bottom + 1):
+15                matrix[row][right] = num
+16                num += 1
+17            right -= 1
+18
+19            if top <= bottom:
+20                for col in range(right, left - 1, -1):
+21                    matrix[bottom][col] = num
+22                    num += 1
+23                bottom -= 1
+24
+25            if left <= right:
+26                for row in range(bottom, top - 1, -1):
+27                    matrix[row][left] = num
+28                    num += 1
+29                left += 1
+30
+31        return matrix
