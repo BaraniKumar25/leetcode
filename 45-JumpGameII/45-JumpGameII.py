bf5b9c4a693d3e710051bc718d04ba468397369b
Barani Kumar <baranikumarsn@gmail.com>
2026-09-03 14:20:23 +0530

    File updated at 9/3/2026, 2:20:22 PM

--- 45-JumpGameII/45-JumpGameII.py
+++ 45-JumpGameII/45-JumpGameII.py
@@ -1,15 +1,33 @@
-# Last updated: 9/3/2026, 2:19:53 PM
+# Last updated: 9/3/2026, 2:20:22 PM
 1class Solution:
-2    def myPow(self, x: float, n: int) -> float:
-3        if n < 0:
-4            x = 1 / x
-5            n = -n
-6
-7        res = 1.0
-8        while n > 0:
-9            if n % 2 == 1:
-10                res *= x
-11            x *= x
-12            n //= 2
-13
-14        return res
+2    def solveNQueens(self, n: int) -> List[List[str]]:
+3        res = []
+4        cols = set()
+5        pos_diag = set()  # (r + c)
+6        neg_diag = set()  # (r - c)
+7
+8        board = [["."] * n for _ in range(n)]
+9
+10        def backtrack(r):
+11            if r == n:
+12                res.append(["".join(row) for row in board])
+13                return
+14
+15            for c in range(n):
+16                if c in cols or (r + c) in pos_diag or (r - c) in neg_diag:
+17                    continue
+18
+19                cols.add(c)
+20                pos_diag.add(r + c)
+21                neg_diag.add(r - c)
+22                board[r][c] = "Q"
+23
+24                backtrack(r + 1)
+25
+26                cols.remove(c)
+27                pos_diag.remove(r + c)
+28                neg_diag.remove(r - c)
+29                board[r][c] = "."
+30
+31        backtrack(0)
+32        return res
