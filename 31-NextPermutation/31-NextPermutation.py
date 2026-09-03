501b796d9cba9b1f569a1ab1775285d018dc06ac
Barani Kumar <baranikumarsn@gmail.com>
2026-09-03 14:10:46 +0530

    File updated at 9/3/2026, 2:10:45 PM

--- 31-NextPermutation/31-NextPermutation.py
+++ 31-NextPermutation/31-NextPermutation.py
@@ -1,23 +1,15 @@
-# Last updated: 9/3/2026, 2:10:13 PM
+# Last updated: 9/3/2026, 2:10:45 PM
 1class Solution:
-2    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-3        candidates.sort()
-4        res = []
-5
-6        def backtrack(start, current_combination, current_sum):
-7            if current_sum == target:
-8                res.append(list(current_combination))
-9                return
-10            if current_sum > target:
-11                return
-12
-13            for i in range(start, len(candidates)):
-14                if i > start and candidates[i] == candidates[i - 1]:
-15                    continue
-16                
-17                current_combination.append(candidates[i])
-18                backtrack(i + 1, current_combination, current_sum + candidates[i])
-19                current_combination.pop()
-20
-21        backtrack(0, [], 0)
-22        return res
+2    def firstMissingPositive(self, nums: List[int]) -> int:
+3        n = len(nums)
+4
+5        for i in range(n):
+6            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+7                correct_idx = nums[i] - 1
+8                nums[i], nums[correct_idx] = nums[correct_idx], nums[i]
+9
+10        for i in range(n):
+11            if nums[i] != i + 1:
+12                return i + 1
+13
+14        return n + 1
