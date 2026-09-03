7eca71e396418b280b9c0e6e8860a8db723beb61
Barani Kumar <baranikumarsn@gmail.com>
2026-09-03 14:48:57 +0530

    File updated at 9/3/2026, 2:48:56 PM

--- 60-PermutationSequence/60-PermutationSequence.py
+++ 60-PermutationSequence/60-PermutationSequence.py
@@ -1,6 +1,18 @@
-# Last updated: 9/3/2026, 2:48:06 PM
-1import math
-2
-3class Solution:
-4    def uniquePaths(self, m: int, n: int) -> int:
-5        return math.comb(m + n - 2, m - 1)
+# Last updated: 9/3/2026, 2:48:56 PM
+1class Solution:
+2    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+3        if not obstacleGrid or obstacleGrid[0][0] == 1:
+4            return 0
+5
+6        m, n = len(obstacleGrid), len(obstacleGrid[0])
+7        dp = [0] * n
+8        dp[0] = 1
+9
+10        for r in range(m):
+11            for c in range(n):
+12                if obstacleGrid[r][c] == 1:
+13                    dp[c] = 0
+14                elif c > 0:
+15                    dp[c] += dp[c - 1]
+16
+17        return dp[-1]
